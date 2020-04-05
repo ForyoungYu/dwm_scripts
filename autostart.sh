ea94582f7a15223b2283430c6da90401813015e1
Salt <1145200512@qq.com>
2020-04-05 18:13:43 +0800

    first edit

---
+++ autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash ~/.scripts/wallpaper.sh &
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-dixde -f -D 5 -c -b
+picom -b
+bash ~/.scripts/dwm-status.sh
+	
+	

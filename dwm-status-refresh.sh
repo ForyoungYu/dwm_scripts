63f6e864c55d4243c7b213040eb62662e72b4535
Salt <1145200512@qq.com>
2020-04-15 09:28:58 +0800

    refresh

--- dwm-status-refresh.sh
+++ dwm-status-refresh.sh
@@ -99,28 +99,26 @@ get_battery_charging_status() {
 	fi
 }
 
-
-
 print_bat(){
-	#hash acpi || return 0
-	#onl="$(grep "on-line" <(acpi -V))"
-	#charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)%"
-	#if test -z "$onl"
-	#then
-		## suspend when we close the lid
-		##systemctl --user stop inhibit-lid-sleep-on-battery.service
-		#echo -e "${charge}"
-	#else
-		## On mains! no need to suspend
-		##systemctl --user start inhibit-lid-sleep-on-battery.service
-		#echo -e "${charge}"
-	#fi
-	#echo "$(get_battery_charging_status) $(get_battery_combined_percent)%, $(get_time_until_charged )";
-	echo "$(get_battery_charging_status) $(get_battery_combined_percent)%$(get_time_until_charged)";
+	# hash acpi || return 0
+	# onl="$(grep "on-line" <(acpi -V))"
+	# charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)%"
+	# if test -z "$onl"
+	# then
+	# 	# suspend when we close the lid
+	# 	systemctl --user stop inhibit-lid-sleep-on-battery.service
+	# 	echo -e "${charge}"
+	# else
+	# 	# On mains! no need to suspend
+	# 	systemctl --user start inhibit-lid-sleep-on-battery.service
+	# 	echo -e "${charge}"
+	# fi
+	# echo "$(get_battery_charging_status) $(get_battery_combined_percent)%, $(get_time_until_charged )";
+	echo "$(get_battery_charging_status) $(get_battery_combined_percent)% $(get_time_until_charged)";
 }
 
 print_date(){
-	date '+%Y-%m-%d %H:%M'
+	date '+%Y-%m-%d %H:%M:%S'
 }
 
 show_record(){
@@ -156,7 +154,7 @@ get_bytes
 vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
 vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
-xsetroot -name " 💿 $(print_mem)M ⬇️ $vel_recv ⬆️ $vel_trans $(dwm_alsa) [ $(print_bat) ]$(show_record) $(print_date) "
+xsetroot -name " MEN $(print_mem)M | ⬇️ $vel_recv ⬆️ $vel_trans | $(dwm_alsa) | <$(print_bat)> | $(show_record) | $(print_date) "
 
 # Update old values to perform new calculations
 old_received_bytes=$received_bytes
